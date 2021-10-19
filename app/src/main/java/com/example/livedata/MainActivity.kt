e7889eea122713aff904176a43318233fada079c
samgashyeong <samgashyeong@naver.com>
2021-10-19 22:21:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
